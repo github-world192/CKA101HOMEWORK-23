f4372e941760a20720d0bb94e583e5c9f5bdbba5
dillen <120437719+github-world192@users.noreply.github.com>
2026-03-26 09:20:36 +0800

    Clean up helloworld class by removing output lines
    Removed print statements from helloworld class.

--- src/thisismyproject/Hw1.java
+++ src/thisismyproject/Hw1.java
@@ -2,9 +2,7 @@ package thisismyproject;
 
 public class helloworld {
 	public static void main(String args[]){
-		System.out.println("a");
-		System.out.println("幹你fff娘");
-		System.out.println("Rrr");//www
+		
 		//qqq
 		///
 	}
