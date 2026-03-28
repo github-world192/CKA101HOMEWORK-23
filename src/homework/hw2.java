d00c6182448188251e4c62324b7b8671ca2867f7
dillen <dillen99999999@gmail.com>
2026-03-28 23:37:43 +0800

    a

---
+++ src/homework/hw2.java
@@ -0,0 +1,11 @@
+package homework;
+//一打12個 200 顆蛋幾打幾顆
+public class hw2 {
+    public static void main(String[] args) {
+        int egg = 200;
+        int dozen = egg / 12;
+        int leftEgg = egg % 12;
+        System.out.println("蛋的打數為" + dozen);
+        System.out.println("剩餘的蛋數為" + leftEgg);
+    }
+}
