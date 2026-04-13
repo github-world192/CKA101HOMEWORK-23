c0041d8823625ef65c31d36713258ccc6d03d50b
dillen <dillen99999999@gmail.com>
2026-04-13 20:34:53 +0800

    some work

---
+++ src/hw3/hw1.java
@@ -0,0 +1,10 @@
+package hw3;
+import java.util.Scanner;
+//• 請設計一隻Java程式,計算1~1000的偶數和 (2+4+6+8+...+1000)
+public class hw1 {
+	public static void main(String[] args) {
+		Scanner qq = new Scanner(System.in);
+        System.out.print("請輸入一個正整數: ");
+		int n = qq.nextInt();
+	}
+}
